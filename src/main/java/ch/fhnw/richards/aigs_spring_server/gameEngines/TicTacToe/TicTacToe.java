ce799847999544dceb75fefb6b90cba6931f7fac
bradley.richards <bradley.richards@fhnw.ch>
2022-10-06 12:52:14 +0200

    Set up game-engine infrastructure, with placeholder for TicTacToe

---
+++ src/main/java/ch/fhnw/richards/aigs_spring_server/gameEngines/TicTacToe/TicTacToe.java
@@ -0,0 +1,16 @@
+package ch.fhnw.richards.aigs_spring_server.gameEngines.TicTacToe;
+
+import java.util.HashMap;
+
+import ch.fhnw.richards.aigs_spring_server.game.Game;
+import ch.fhnw.richards.aigs_spring_server.gameEngines.GameEngine;
+
+public class TicTacToe implements GameEngine {
+
+	@Override
+	public Game move(Game game, HashMap<String, String> move) {
+		// TODO Auto-generated method stub
+		return game;
+	}
+
+}
--- src/main/java/ch/fhnw/richards/aigs_spring_server/user/User.java
+++ src/main/java/ch/fhnw/richards/aigs_spring_server/user/User.java
@@ -75,7 +75,7 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "Room{" + "userName=" + this.userName + '}';
+		return "User{" + "userName=" + this.userName + '}';
 	}
 
 }
