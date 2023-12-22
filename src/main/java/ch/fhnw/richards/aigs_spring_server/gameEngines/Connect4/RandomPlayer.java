48ee04e114a928625db108ad1098486d30654ae2
severin <sev.nyffenegger@gmail.com>
2023-12-22 10:22:55 +0100

    Zusammen: Connect4 move method adaptation

--- src/main/java/ch/fhnw/richards/aigs_spring_server/gameEngines/Connect4/RandomPlayer.java
+++ src/main/java/ch/fhnw/richards/aigs_spring_server/gameEngines/Connect4/RandomPlayer.java
@@ -1,6 +1,38 @@
 package ch.fhnw.richards.aigs_spring_server.gameEngines.Connect4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RandomPlayer implements C4_AI {
+
+    @Override
+    public void makeMove(long[][] board) {
+        List<Integer> availableColumns = new ArrayList<>();
+        for (int col = 0; col < board[0].length; col++) {
+            if (board[0][col] == 0) { // Check if the top of the column is empty
+                availableColumns.add(col);
+            }
+        }
+
+        if (!availableColumns.isEmpty()) {
+            Random rand = new Random();
+            int col = availableColumns.get(rand.nextInt(availableColumns.size()));
+            int row = findFirstEmptyRow(board, col);
+            board[row][col] = -1; // Assuming -1 is the AI's piece
+        }
+    }
+
+    private int findFirstEmptyRow(long[][] board, int col) {
+        for (int row = board.length - 1; row >= 0; row--) {
+            if (board[row][col] == 0) {
+                return row;
+            }
+        }
+        return -1; // Should not happen if called correctly
+    }
+
+    /*
     @Override
     public void makeMove(long[][] board) {
         boolean found = false;
@@ -13,4 +45,5 @@ public class RandomPlayer implements C4_AI {
             }
         }
     }
+     */
 }
